7cd02e49a383fd55f2907e038ab7b5a5175be473
garfield <cmgarfield@gmail.com>
2018-09-02 15:03:59 +0800

    customize prefix for neater path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
-  get 'leadgen/advertising/asdf/lead', to: 'pages#contact'
+  get 'leadgen/advertising/asdf/lead', to: 'pages#contact', as: 'lead'
 
   resources :blogs
 end
