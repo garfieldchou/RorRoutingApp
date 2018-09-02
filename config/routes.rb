640662ca8a79539f2f64d5d48315947e30bc1a0d
garfield <cmgarfield@gmail.com>
2018-09-02 14:30:00 +0800

    customize another name of route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
-  get 'contact', to: 'pages#contact'
+  get 'leadgen/advertising/asdf/lead', to: 'pages#contact'
 
   resources :blogs
 end
