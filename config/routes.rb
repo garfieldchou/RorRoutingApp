a1ba7ea5182ae449eb23b40de5fe6633f762b26c
garfield <cmgarfield@gmail.com>
2018-09-02 22:40:47 +0800

    generate Dashboard items, $ rails g controller Dashboard main user blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'dashboard/main'
+  get 'dashboard/user'
+  get 'dashboard/blog'
   get 'about', to: 'pages#about'
 
   get 'leadgen/advertising/asdf/lead', to: 'pages#contact', as: 'lead'
